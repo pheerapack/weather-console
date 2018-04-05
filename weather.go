1110aca840c77dc22d5b58eee68ccbbaf732dde8
pheerapack <h.deleted.p@gmail.com>
2018-04-05 10:45:19 +0700

    change variable

--- weather.go
+++ weather.go
@@ -10,11 +10,9 @@ var bot = [10]string{"|_|", "  |", "|_ ", " _|", "  |", " _|", "|_|", "  |", "|_
 var fourtyeight = 48
 
 func toDigital(number int) string {
-
 	str := strconv.Itoa(number)
 	palldigit := finddigit(str)
 	return palldigit
-
 }
 
 func finddigit(str string) string {
@@ -25,9 +23,9 @@ func finddigit(str string) string {
 
 	for i := range str {
 		digit = int(str[i])
-		alltop = alltop + top[digit-fourtyeight]
-		allmid = allmid + mid[digit-fourtyeight]
-		allbot = allbot + bot[digit-fourtyeight]
+		alltop += top[digit-fourtyeight]
+		allmid += mid[digit-fourtyeight]
+		allbot += bot[digit-fourtyeight]
 	}
 	alldigit := alltop + "\n" + allmid + "\n" + allbot
 	return alldigit
